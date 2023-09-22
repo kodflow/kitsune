d930203ed3d582ca88e09073a2a9f13915a8b519
Florent <contact@making.codes>
2023-09-23 00:20:52 +0200

    feat(update): update project for test DockerImage Build

--- src/internal/core/server/protocols/socket/server.go
+++ src/internal/core/server/protocols/socket/server.go
@@ -15,7 +15,6 @@ import (
 	"net"
 	"runtime"
 
-	"github.com/kodmain/kitsune/src/config"
 	"github.com/kodmain/kitsune/src/internal/core/server/router"
 	"github.com/kodmain/kitsune/src/internal/core/server/transport"
 	"github.com/kodmain/kitsune/src/internal/kernel/observability/logger"
@@ -58,7 +57,7 @@ func (s *Server) Start() error {
 
 	connections := make(chan net.Conn, runtime.NumCPU())
 
-	for i := 0; i < runtime.NumCPU()*config.DEFAULT_IO_BOUND; i++ {
+	for i := 0; i < runtime.NumCPU(); i++ {
 		go s.worker(connections)
 	}
 
